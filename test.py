efaabe93a82d8e70d377de4b4202350bbe83538c
Sai Siddartha Maram <smaram_be16@thapar.edu>
2019-06-03 22:23:35 +0530

    generic

--- test.py
+++ test.py
@@ -4,25 +4,40 @@ import numpy as np
 
 img= cv2.imread('0015561.jpg')
 
+#to display image and take in points
 plt.figure()
 plt.imshow(img)
 plt.show()
 
-cv2.circle(img,(292,994),15,(0,255,255),4)
-cv2.circle(img,(564,763),15,(0,255,255),4)
-cv2.circle(img,(804,763),15,(0,255,255),4)
-cv2.circle(img,(1008,956),15,(0,255,255),4)
 
-cv2.line(img,(292,994),(564,763),(0,0,0),3)
-cv2.line(img,(292,994),(1008,956),(0,0,0),3)
-cv2.line(img,(564,763),(804,763),(0,0,0),3)
-cv2.line(img,(804,763),(1008,956),(0,0,0),3)
+bl_x=int(input('please enter bottom left x'))
+bl_y=int(input('please enter bottom left y'))
+
+tl_x=int(input('please enter top left x'))
+tl_y=int(input('please enter top left y'))
+
+tr_x=int(input('please enter top right x'))
+tr_y=int(input('please enter top right y'))
+
+br_x=int(input('please enter bottom right x'))
+br_y=int(('please enter bottom right y'))
+
+#
+cv2.circle(img,(bl_x,bl_y),15,(0,255,255),4)
+cv2.circle(img,(tl_x,tl_y),15,(0,255,255),4)
+cv2.circle(img,(tr_x,tr_y),15,(0,255,255),4)
+cv2.circle(img,(br_x,br_y),15,(0,255,255),4)
+
+cv2.line(img,(bl_x,bl_y),(tl_x,tl_y),(0,0,0),3)
+cv2.line(img,(tl_x,tl_y),(tr_x,tr_y),(0,0,0),3)
+cv2.line(img,(tr_x,tr_y),(br_x,br_y),(0,0,0),3)
+cv2.line(img,(br_x,br_y),(bl_x,bl_y),(0,0,0),3)
 
 h,w,_=img.shape
 cv2.imwrite('points.jpg',img)
 
-source_points=np.float32([ [292,994],[564,763],[804,763],[1008,956]  ])
-destination_points = np.float32([ [0,1700], [0, 0], [230, 0], [230, 1700] ])
+source_points=np.float32([ [bl_x,bl_y],[tl_x,tl_y],[tr_x,tr_y],[br_x,br_y]  ])
+destination_points = np.float32([ [0,1700], [0, 0], [300, 0], [300, 1700] ])
 
 
 image=cv2.imread('points.jpg')
--- test_case_2.py
+++ test_case_2.py
@@ -2,30 +2,30 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-img= cv2.imread('0018768.jpg')
+img= cv2.imread('0018705.jpg')
 
 plt.figure()
 plt.imshow(img)
 plt.show()
 
-cv2.circle(img,(412,985),15,(0,0,255))
-cv2.circle(img,(588,777),15,(0,255,0))
-cv2.circle(img,(848,777),15,(0,255,255))
-cv2.circle(img,(1116,985),15,(0,0,0))
+cv2.circle(img,(292,994),15,(0,255,255),4)
+cv2.circle(img,(564,763),15,(0,255,255),4)
+cv2.circle(img,(804,763),15,(0,255,255),4)
+cv2.circle(img,(1008,956),15,(0,255,255),4)
 
-cv2.line(img,(412,985),(588,777),(0,0,0),3)
-cv2.line(img,(848,777),(588,777),(0,0,0),3)
-cv2.line(img,(848,777),(1116,985),(0,0,0),3)
-cv2.line(img,(1116,991),(412,985),(0,0,0),3)
+cv2.line(img,(292,994),(564,763),(0,0,0),3)
+cv2.line(img,(292,994),(1008,956),(0,0,0),3)
+cv2.line(img,(564,763),(804,763),(0,0,0),3)
+cv2.line(img,(804,763),(1008,956),(0,0,0),3)
 
 h,w,_=img.shape
 cv2.imwrite('points2.jpg',img)
 
-source_points=np.float32([ [412,985],[588,777],[848,777],[1116,985]  ])
-destination_points = np.float32([ [0,1700], [0, 0], [300, 0], [300, 1700] ])
+source_points=np.float32([ [292,994],[564,763],[804,763],[1008,956]  ])
+destination_points = np.float32([ [0,1700], [0, 0], [230, 0], [230, 1700] ])
 
 
-image=cv2.imread('points2.jpg')
+image=cv2.imread('points.jpg')
 matrix = cv2.getPerspectiveTransform(source_points, destination_points)
 result = cv2.warpPerspective(image, matrix, (300,1700))
 plt.figure()
@@ -33,16 +33,10 @@ plt.imshow(result)
 plt.show()
 cv2.imwrite("birdie_test_case_2.jpg",result)
 
-#mtx=[[2468.6668434782608,0,1228.876620888020],[0,2468.6668434782608,1012.976060035710],[0,0,1]] 
-#dist=[ 0.00125859 , 0 ,  -0.00010658,0 ]
-
-mtx=[[1203.032354,0,720.0],[0,1284.609285,540.0],[0,0,1]] 
-dist=[ 0 , 0 ,  0,0 ]
-
-
+mtx=[[2468.6668434782608,0,1228.876620888020],[0,2468.6668434782608,1012.976060035710],[0,0,1]] 
+dist=[ 0.00125859 , 0 ,  -0.00010658,0 ]
 
 ppx=300/3.6
-print(ppx)
 Lh = np.linalg.inv(np.matmul(matrix, mtx))
 pix_per_meter_y = ppx * np.linalg.norm(Lh[:,0]) / np.linalg.norm(Lh[:,1])
 print(ppx, pix_per_meter_y)
